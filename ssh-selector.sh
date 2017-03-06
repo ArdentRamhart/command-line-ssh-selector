63678fe7725f51b1f49f6942557b98bfcd90178a
Ardent Ramhart <ardentramhart@gmail.com>
2017-03-06 15:43:07 -0800

    got printing of server names to work with jq

--- ssh-selector.sh
+++ ssh-selector.sh
@@ -37,21 +37,29 @@ fi
 
 
 # open List as a FD
-exec 3< "$ssh_machine_list_path"
+#exec 3< "$ssh_machine_list_path"
 
 # Read in list as an array
-readarray -u 3 ssh_machine_array
+#readarray -u 3 ssh_machine_array
 
 # Close FD
-exec 3<&-
+#exec 3<&-
 
 # loop count for printing selection
-loop_count=0
+server_count=`jq '. | length' $ssh_machine_list_path`
+if $verbose_output; then
+  echo "INFO: current machine list length : $server_count"
+fi
 
 # print out list with associated numbers
-for ssh_address in ${ssh_machine_array[@]}; do
-  echo "$loop_count : $ssh_address"
-  let "loop_count+=1"
+for ((i=0; i<$server_count; i++)); do
+  server_name=$( jq ".[$i]["\""serverName"\""]" $ssh_machine_list_path )
+  list_output="$i : $server_name"
+  if $verbose_output; then
+    server_ip=$( jq ".[$i]["\""ip"\""]" $ssh_machine_list_path )
+    list_output="$list_output | ip: $server_ip"
+  fi
+  echo "$list_output"
 done
 
 # print out message for user input
@@ -60,8 +68,19 @@ echo -n "Select Machine to SSH into : "
 # read user input and save to varibable
 read ssh_machine_selection
 
+# reading the server name and ip from the list
+server_name=$(jq ".[$ssh_machine_selection]["\""serverName"\""]" $ssh_machine_list_path )
+server_ip=$(jq ".[$ssh_machine_selection]["\""ip"\""]" $ssh_machine_list_path )
+
+# stripping double quotes from ip and server name
+server_name=${server_name:1:-1}
+server_ip=${server_ip:1:-1}
+
 # print out confirmation of selection
-echo "you selected : ${ssh_machine_array[$ssh_machine_selection]}-- Connecting Now ..."
+echo "you selected : $server_name -- Connecting Now ..."
+if $verbose_output; then
+  echo "INFO: selected machine ip : $server_ip"
+fi
 
 # connect to selected machine
-ssh "${ssh_machine_array[$ssh_machine_selection]}"
+ssh $server_ip
